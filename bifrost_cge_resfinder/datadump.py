e088fc5e1a7bd18e8e9f385b0f9d0e6407a12251
githubz0r <lc@MacBook-Pro-4.local>
2021-10-25 14:15:29 +0200

    changed databases to database_versions

--- bifrost_cge_resfinder/datadump.py
+++ bifrost_cge_resfinder/datadump.py
@@ -19,7 +19,7 @@ def extract_resistance(resistance: Category, results: Dict, component_name: str)
     seq_regions = results_json['seq_regions']
     seq_variations = results_json['seq_variations']
     dbs = results_json['databases']
-    resistance['databases'] = [{"name":dbs[i]['database_name'], "version":dbs[i]['database_version']} for i in dbs.keys()]
+    resistance['database_versions'] = {dbs[i]['database_name']:dbs[i]['database_version'] for i in dbs.keys()}
     # collect items for resistant phenotypes
     for phenotype_key in phenotypes.keys():
         phenotype = phenotypes[phenotype_key]
@@ -61,36 +61,7 @@ def extract_resistance(resistance: Category, results: Dict, component_name: str)
                     }
                     report_dict['seq_regions'].append(seq_region_dict)
             resistance['report']['data'].append(report_dict)
-    #phen_to_gene_map = {phen:[] for phen in phenotypes.keys()}
-    # for gene in genes.keys():
-    #     gene_phenotype = genes[gene]["phenotypes"]
-    #     if len(gene_phenotype) > 0:
-    #         for phen in gene_phenotype:
-    #                 phen_to_gene_map[phen].append(gene)
-    # for phenotype in phenotypes.keys():
-    #     if phenotypes[phenotype]['resistant']==True:
-    #         phen_dict = {"phenotype":phenotype}
-    #         phen_dict['seq_variations'] = ", ".join(phenotypes[phenotype]['seq_variations'])
-    #         phen_dict['genes'] = ", ".join([genes[i]['name'] for i in phen_to_gene_map[phenotype]])
-    #         phen_dict['amr_classes'] = ",".join(phenotypes[phenotype]['amr_classes'])
-    #         resistance['summary']['phenotypes'].append(phen_dict)
-    #         # make a report with gene coverage and shit
-    #         report_dict = {"phenotype":phenotype}
-    #         report_seq_variations = phenotypes[phenotype]['seq_variations']
-    #         seq_variation_dicts = [{"key":results_json['seq_variations'][i]['key'], 
-    #                                "seq_var":results_json['seq_variations'][i]['seq_var'],
-    #                                "genes": seq_variation_gene_object(results_json['seq_variations'][i]['genes'], results_json)} for i in report_seq_variations]
-            # report_dict["seq_variations"] = seq_variation_dicts
-            # report_dict['genes'] = []
-            # for gene_dict in [genes[i] for i in phen_to_gene_map[phenotype]]:
-            #     key = gene_dict['key']
-            #     name = gene_dict['name']
-            #     coverage = gene_dict['coverage']
-            #     identity = gene_dict['identity']
-            #     report_dict['genes'].append({"key":key, "name":name, "coverage":coverage, "identity":identity})
-            # report_dict['amr_classes'] = phenotypes[phenotype]['amr_classes']
-            # resistance['report']['data'].append(report_dict)
-    #print(resistance)
+
 def seq_variation_gene_object(gene_list, resfinder_json):
     for gene in gene_list:
         key = resfinder_json['genes'][gene]['key']
